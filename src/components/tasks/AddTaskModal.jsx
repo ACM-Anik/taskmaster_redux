d208b5b4fe468d16241d78acc90e9cedf4801ad5
ACM-Anik <anikmojumder5252@gmail.com>
2024-02-24 23:22:58 +0600

    React-hook-form and tailwind form plugin done. Also a initial form created

--- src/components/tasks/AddTaskModal.jsx
+++ src/components/tasks/AddTaskModal.jsx
@@ -1,13 +1,31 @@
+import { useForm } from "react-hook-form";
 import Modal from "../ui/Modal";
 
-const AddTaskModal = ({isOpen, setIsOpen}) => {
+const AddTaskModal = ({ isOpen, setIsOpen }) => {
+
+    const { register, handleSubmit } = useForm();
+
+    const onSubmit = (data) => {
+        console.log(data);
+    }
+
     return (
         <Modal
             isOpen={isOpen}
             setIsOpen={setIsOpen}
             title={"Programming Hero"}
         >
-            <p>Children</p>
+            <form onSubmit={handleSubmit(onSubmit)}>
+                <div className="flex flex-col gap-3">
+                    <label htmlFor="title">Title</label>
+                    <input
+                        className="w-full rounded-md" type="text"
+                        id="title"
+                        {...register('Name')}
+                    />
+                </div>
+                <button type="submit">Submit</button>
+            </form>
         </Modal>
     );
 };
