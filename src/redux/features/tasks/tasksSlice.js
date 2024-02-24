31815858b5af0390ab4d74affbc0e8080c58950f
ACM-Anik <anikmojumder5252@gmail.com>
2024-02-25 01:54:02 +0600

    To maintain the serial of task element, if-else condition used.

--- src/redux/features/tasks/tasksSlice.js
+++ src/redux/features/tasks/tasksSlice.js
@@ -8,10 +8,19 @@ const tasksSlice = createSlice({
     initialState,
     reducers: {
         addTask: (state, { payload }) => {
-            state.tasks.push(payload);
+            if (state.tasks.length === 0) {
+                state.tasks.push({id: 1, status: 'pending', ...payload});
+            }
+            else{ //.at(-1): This attempts to retrieve the element at the last index of the tasks array. The negative index -1 indicates counting from the end of the array. However, there's an important point to note: the .at() method does not modify the original array; it returns the element at the specified index without changing the array itself.
+                const lastElement = state.tasks.at(-1);
+                state.tasks.push({
+                    id: lastElement.id + 1, 
+                    status: 'pending', 
+                    ...payload});
+            }
         }
     },
 });
 
-export const {addTask} = tasksSlice.actions;
+export const { addTask } = tasksSlice.actions;
 export default tasksSlice.reducer;
