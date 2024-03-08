a53a9353daacbbac988266b1fef4654234af61c3
ACM-Anik <anikmojumder5252@gmail.com>
2024-03-09 00:18:34 +0600

    extraReducers implemented with builder and 3 .addCase as pending, fulfilled & rejected done

--- src/redux/features/users/usersSlice.js
+++ src/redux/features/users/usersSlice.js
@@ -3,8 +3,8 @@ import { createUserWithEmailAndPassword } from "firebase/auth";
 import auth from "../../../utils/firebase.config";
 
 const initialState = {
-    name: 'ACM Anik',
-    email: 'anikmojumder@gmail.com',
+    name: '',
+    email: '',
     isLoading: true,
     isError: false,
     error: '',
@@ -15,14 +15,44 @@ export const createUser = createAsyncThunk("usersSlice/createUser", async ({ ema
 
     console.log(data);
 
-    return;
+    return ;
 })
 
 const usersSlice = createSlice({
     name: 'usersSlice',
     initialState,
-    reducers: {
+    reducers: {},
 
+    extraReducers: (builder) => {
+        /* There are two type of code structure:
+        builder.addCase();
+        builder.addCase();
+        builder.addCase();
+        or chaining,
+        builder.addCase().addCase().addCase();
+        */
+        builder
+            .addCase(createUser.pending, (state) => {
+                state.isLoading = true;
+                state.isError = false;
+                state.email = '';
+                state.name = '';
+                state.error = '';
+            })
+            .addCase(createUser.fulfilled, (state, {payload}) => {
+                state.isLoading = false;
+                state.isError = false;
+                state.email = payload.email;
+                state.name = payload.name;
+                state.error = '';
+            })
+            .addCase(createUser.rejected, (state, action) => {
+                state.isLoading = false;
+                state.isError = true;
+                state.email = '';
+                state.name = '';
+                state.error = action.error.message;
+            })
     },
 });
 
