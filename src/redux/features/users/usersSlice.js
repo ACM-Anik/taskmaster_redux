65f524270e0e7058891629e4192ed8f89666a42b
ACM-Anik <anikmojumder5252@gmail.com>
2024-03-08 23:53:40 +0600

    AsyncThunk of redux initially created in the usersSlice.js to handle firebase authentication

--- src/redux/features/users/usersSlice.js
+++ src/redux/features/users/usersSlice.js
@@ -1,9 +1,22 @@
-import { createSlice } from "@reduxjs/toolkit";
+import { createAsyncThunk, createSlice } from "@reduxjs/toolkit";
+import { createUserWithEmailAndPassword } from "firebase/auth";
+import auth from "../../../utils/firebase.config";
 
 const initialState = {
     name: 'ACM Anik',
     email: 'anikmojumder@gmail.com',
+    isLoading: true,
+    isError: false,
+    error: '',
 };
+
+export const createUser = createAsyncThunk("usersSlice/createUser", async ({ email, password }) => {
+    const data = await createUserWithEmailAndPassword(auth, email, password);
+
+    console.log(data);
+
+    return;
+})
 
 const usersSlice = createSlice({
     name: 'usersSlice',
