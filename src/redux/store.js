cbb878df37bd5e05684fa805281da5360647bb4a
ACM-Anik <anikmojumder5252@gmail.com>
2024-03-11 23:17:59 +0600

    baseApi created to call the server to get the data

--- src/redux/store.js
+++ src/redux/store.js
@@ -1,9 +1,11 @@
 import { configureStore } from '@reduxjs/toolkit';
 import tasksSlice from './features/tasks/tasksSlice';
 import usersSlice from './features/users/usersSlice';
+import baseApi from './features/api/baseApi';
 
 const store = configureStore({
   reducer: {
+    [baseApi.reducer]: baseApi.reducerPath,
     tasksSlice: tasksSlice,
     usersSlice: usersSlice,
   },
