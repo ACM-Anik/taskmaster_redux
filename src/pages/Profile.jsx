ba64d229f60abf74a2b853e5d40f5b35b793d651
ACM-Anik <anikmojumder5252@gmail.com>
2024-03-26 23:55:56 +0600

    DeleteUser of firbase implemented and a toast added

--- src/pages/Profile.jsx
+++ src/pages/Profile.jsx
@@ -1,7 +1,8 @@
 import { useEffect, useState } from 'react';
-import { sendEmailVerification, updateEmail, updateProfile } from 'firebase/auth';
+import { deleteUser, sendEmailVerification, updateEmail, updateProfile } from 'firebase/auth';
 import { useSelector } from 'react-redux';
 import auth from '../utils/firebase.config';
+import toast from 'react-hot-toast';
 
 
 const Profile = () => {
@@ -59,8 +60,56 @@ const Profile = () => {
     }
   };
 
+  const [click, setClick] = useState(null);
+
   const handleDelete = () => {
-    console.log('Delete', 'Delete');
+    const user = auth.currentUser;
+
+    // Toast to Delete:-
+    toast.custom((t) => (
+      <div className={`${t.visible ? 'animate-enter' : 'animate-leave'
+        } max-w-md w-full bg-white shadow-lg rounded-lg pointer-events-auto flex ring-1 ring-black ring-opacity-5`}
+      >
+        <div className="flex-1 w-0 p-4">
+          <div className="flex items-start">
+            <div className="flex-shrink-0 pt-0.5">
+              <img
+                className="h-10 w-10 rounded-full"
+                src="https://images.unsplash.com/photo-1494790108377-be9c29b29330?ixlib=rb-1.2.1&ixqx=6GHAjsWpt9&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=facearea&facepad=2.2&w=160&h=160&q=80"
+                alt=""
+              />
+            </div>
+            <div className="ml-3 flex-1">
+              <p className="text-sm font-medium text-gray-900">
+                Emilia Gates
+              </p>
+              <p className="mt-1 text-sm text-gray-500">
+                Are you sure? want to delete your account?
+              </p>
+            </div>
+          </div>
+        </div>
+        <div className="flex border-l border-gray-200">
+          <button
+            onClick={() => [toast.dismiss(t.id), setClick(1)]}
+            className="w-full border border-transparent rounded-none rounded-r-lg p-4 flex items-center justify-center text-sm font-medium text-indigo-600 hover:text-indigo-500 focus:outline-none focus:ring-2 focus:ring-indigo-500"
+          >
+            Delete
+          </button>
+        </div>
+      </div>
+    ));
+
+    if(click){
+      deleteUser(user).then(() => {
+        // User deleted.
+      }).catch((error) => {
+        // An error ocurred
+        // ...
+        console.log('Delete Error', error);
+      });
+    }
+
   }
 
 
