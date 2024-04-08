f5a489165e4651a6f3bb35e02b8120632f936bc8
ACM-Anik <anikmojumder5252@gmail.com>
2024-04-08 21:06:13 +0600

    Made a array of images and used map in the Chat Contracts in Chat page

--- src/pages/Chat.jsx
+++ src/pages/Chat.jsx
@@ -18,6 +18,15 @@ const Chat = () => {
     });
   }, []);
 
+  const images = [
+    "https://images.unsplash.com/photo-1528892952291-009c663ce843?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=644&q=80",
+    "https://images.unsplash.com/photo-1506794778202-cad84cf45f1d?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=1974&q=80",
+    "https://images.unsplash.com/photo-1494790108377-be9c29b29330?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=1974&q=80",
+    "https://images.unsplash.com/photo-1552374196-c4e7ffc6e126?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=1974&q=80",
+    "https://images.unsplash.com/photo-1507003211169-0a1dd7228f2d?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=1974&q=80",
+    "https://images.unsplash.com/photo-1500648767791-00dcc994a43e?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=1974&q=80"
+  ];
+
 
   return (
     <>
@@ -50,7 +59,6 @@ const Chat = () => {
                   />
                 </div>
               </MenuDropdown>
-
             </div>
           </div>
           <div className="grid grid-cols-1 gap-5 mt-10">
@@ -73,66 +81,22 @@ const Chat = () => {
         </div>
 
         <div className="col-span-3 border-l-2 border-secondary/20 px-6 pt-10">
-          
-            <h1 className="text-xl text-center">Chat Contacts</h1>
-            <div className="flex flex-col items-center gap-3 mt-3">
-
-              <div className="bg-blue-200 p-2 rounded-lg flex items-center justify-center">
+          <h1 className="text-xl text-center">Chat Contacts</h1>
+          <div className="flex flex-col items-center gap-3 mt-3">
+            {images?.map((member) =>
+              <div key={member} className="bg-blue-200 p-2 rounded-lg flex items-center justify-center">
                 <div className="h-10 w-10 m-2 rounded-xl overflow-hidden">
                   <img
-                    src="https://images.unsplash.com/photo-1506794778202-cad84cf45f1d?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=1974&q=80"
-                    alt=""
-                    className="object-cover h-full w-full "
+                    src={member}
+                    alt="member"
+                    className="object-cover h-full w-full"
                   />
                 </div>
-                <h1 className="text-xl">Name</h1>
+                <h1 className="text-lg font-semibold">Name</h1>
               </div>
+            )};
+          </div>
 
-              <div className="bg-blue-200 p-2 rounded-lg flex items-center justify-center">
-                <div className="h-10 w-10 m-2 rounded-xl overflow-hidden">
-                  <img
-                    src="https://images.unsplash.com/photo-1494790108377-be9c29b29330?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=1974&q=80"
-                    alt=""
-                    className="object-cover h-full w-full "
-                  />
-                </div>
-                <h1 className="text-xl">Name</h1>
-              </div>
-
-              <div className="bg-blue-200 p-2 rounded-lg flex items-center justify-center">
-                <div className="h-10 w-10 m-2 rounded-xl overflow-hidden">
-                  <img
-                    src="https://images.unsplash.com/photo-1552374196-c4e7ffc6e126?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=1974&q=80"
-                    alt=""
-                    className="object-cover h-full w-full "
-                  />
-                </div>
-                <h1 className="text-xl">Name</h1>
-              </div>
-
-              <div className="bg-blue-200 p-2 rounded-lg flex items-center justify-center">
-                <div className="h-10 w-10 m-2 rounded-xl overflow-hidden">
-                  <img
-                    src="https://images.unsplash.com/photo-1507003211169-0a1dd7228f2d?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=1974&q=80"
-                    alt=""
-                    className="object-cover h-full w-full "
-                  />
-                </div>
-                <h1 className="text-xl">Name</h1>
-              </div>
-
-              <div className="bg-blue-200 p-2 rounded-lg flex items-center justify-center">
-                <div className="h-10 w-10 m-2 rounded-xl overflow-hidden">
-                  <img
-                    src="https://images.unsplash.com/photo-1500648767791-00dcc994a43e?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=1974&q=80"
-                    alt=""
-                    className="object-cover h-full w-full "
-                  />
-                </div>
-                <h1 className="text-xl">Name</h1>
-              </div>
-            </div>
-          
         </div>
       </div>
     </>
