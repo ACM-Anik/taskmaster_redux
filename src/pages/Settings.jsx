3ae2d59ac93a5b98428733851512c00e282d19f0
ACM-Anik <anikmojumder5252@gmail.com>
2024-04-14 23:43:45 +0600

    Scrolling issue of settings solved

--- src/pages/Settings.jsx
+++ src/pages/Settings.jsx
@@ -1,13 +1,11 @@
 import { BellIcon, MagnifyingGlassIcon } from '@heroicons/react/24/outline';
 import { useEffect, useState } from 'react';
 import MenuDropdown from '../components/ui/MenuDropdown';
-import { useGetTasksQuery } from '../redux/features/tasks/taskApi';
-import { onAuthStateChanged } from 'firebase/auth';
 import auth from '../utils/firebase.config';
+import { onAuthStateChanged } from 'firebase/auth';
 import { useGetUsersQuery } from '../redux/features/users/usersApi';
 
 const Settings = () => {
-  const { data: tasks } = useGetTasksQuery();
   const { data: allUsers } = useGetUsersQuery();
   // Setting the user Profile:-
   const [user, setUser] = useState();
@@ -22,13 +20,14 @@ const Settings = () => {
 
   return (
     <>
-      <div className="h-screen grid grid-cols-12">
+      <div className="h-screen overflow-hidden grid grid-cols-12">
         <div className="col-span-9 px-10 pt-10">
+          {/* NavBars-------------------- */}
           <div className="flex justify-between items-center">
             <div>
               <h1 className="font-semibold text-3xl">Setting</h1>
             </div>
-
+            {/* NavLinks  */}
             <div className="flex gap-5">
               <button className="border-2 border-secondary/20 hover:border-primary hover:bg-primary rounded-xl h-10 w-10  grid place-content-center text-secondary hover:text-white transition-all">
                 <MagnifyingGlassIcon className="h-6 w-6" />
@@ -55,14 +54,14 @@ const Settings = () => {
             </div>
           </div>
           <div className="grid grid-cols-1 gap-5 mt-10">
-            <div className="relative h-[800px] overflow-auto">
+            <div className="relative h-screen overflow-y-auto">
               <div className="flex sticky top-0 justify-between bg-[#D3DDF9] p-5 rounded-md mb-3">
                 <h1>Total Members (Users)</h1>
                 <p className="bg-primary text-white w-6 h-6 grid place-content-center rounded-md">
-                  {tasks?.length}
+                  {allUsers?.length}
                 </p>
               </div>
-              <div className="space-y-3">
+              <div className="space-y-3 overflow-x-hidden overflow-y-auto mb-36">
                 {allUsers?.map((user) =>
                   <div  key={user._id} className="bg-sky-200 p-3 rounded-lg flex items-center">
                     <div className="h-10 w-10 m-2 rounded-xl overflow-hidden">
@@ -72,7 +71,10 @@ const Settings = () => {
                         className="object-cover h-full w-full"
                       />
                     </div>
-                    <h1 className="text-xl">{user.name}</h1>
+                    <div className="">
+                    <h1 className="text-lg font-semibold">{user.name}</h1>
+                    <h1 className="text-lg">{user.email}</h1>
+                    </div>
                   </div>
                 )}
               </div>
@@ -80,7 +82,7 @@ const Settings = () => {
           </div>
         </div>
 
-        <div className="col-span-3 border-l-2 border-secondary/20 px-10 pt-10">
+        <div className="col-span-3 border-l-2 border-secondary/20 px-10 pt-10 overflow-hidden">
           <div>
             <h1 className="text-xl">Shortcut Tools</h1>
             <div className="flex flex-wrap items-center gap-3 mt-3 overflow-y-auto overflow-x-hidden">
