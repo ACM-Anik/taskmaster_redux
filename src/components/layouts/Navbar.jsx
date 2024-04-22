7e92cc2dbb0b4269f0116ed70868daeae77b8a64
ACM-Anik <anikmojumder5252@gmail.com>
2024-04-22 23:38:11 +0600

    successfully used the Navbar in the Settings page

--- src/components/layouts/Navbar.jsx
+++ src/components/layouts/Navbar.jsx
@@ -1,11 +1,11 @@
 import { BellIcon, MagnifyingGlassIcon } from '@heroicons/react/24/outline';
 import { useEffect, useState } from 'react';
-import MenuDropdown from '../components/ui/MenuDropdown';
-import auth from '../utils/firebase.config';
+import MenuDropdown from '../ui/MenuDropdown';
 import { onAuthStateChanged } from 'firebase/auth';
+import auth from '../../utils/firebase.config';
 
 
-const Navbar = () => {
+const Navbar = ({title}) => {
     // Setting the user Profile:-
     const [user, setUser] = useState();
     useEffect(() => {
@@ -18,31 +18,37 @@ const Navbar = () => {
 
 
     return (
-        <div>
-            <div className="flex gap-5">
-                <button className="border-2 border-secondary/20 hover:border-primary hover:bg-primary rounded-xl h-10 w-10  grid place-content-center text-secondary hover:text-white transition-all">
-                    <MagnifyingGlassIcon className="h-6 w-6" />
-                </button>
-                <button className="border-2 border-secondary/20 hover:border-primary hover:bg-primary rounded-xl h-10 w-10 grid place-content-center text-secondary hover:text-white transition-all">
-                    <BellIcon className="h-6 w-6" />
-                </button>
+        <>
+            <div className="flex justify-between items-center">
+                <div>
+                    <h1 className="font-semibold text-3xl">{title}</h1>
+                </div>
+                {/* NavLinks/Buttons  */}
+                <div className="flex gap-5">
+                    <button className="border-2 border-secondary/20 hover:border-primary hover:bg-primary rounded-xl h-10 w-10  grid place-content-center text-secondary hover:text-white transition-all">
+                        <MagnifyingGlassIcon className="h-6 w-6" />
+                    </button>
+                    <button className="border-2 border-secondary/20 hover:border-primary hover:bg-primary rounded-xl h-10 w-10 grid place-content-center text-secondary hover:text-white transition-all">
+                        <BellIcon className="h-6 w-6" />
+                    </button>
 
-                <MenuDropdown>
-                    <div className="h-10 w-10 rounded-xl overflow-hidden">
-                        <img
-                            title={user?.displayName}
-                            src={user?.photoURL ?
-                                user.photoURL
-                                :
-                                "https://images.unsplash.com/photo-1528892952291-009c663ce843?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=644&q=80"
-                            }
-                            alt="profile"
-                            className="object-cover h-full w-full "
-                        />
-                    </div>
-                </MenuDropdown>
+                    <MenuDropdown>
+                        <div className="h-10 w-10 rounded-xl overflow-hidden">
+                            <img
+                                title={user?.displayName}
+                                src={user?.photoURL ?
+                                    user.photoURL
+                                    :
+                                    "https://images.unsplash.com/photo-1528892952291-009c663ce843?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=644&q=80"
+                                }
+                                alt="profile"
+                                className="object-cover h-full w-full "
+                            />
+                        </div>
+                    </MenuDropdown>
+                </div>
             </div>
-        </div>
+        </>
     );
 };
 
--- src/pages/Settings.jsx
+++ src/pages/Settings.jsx
@@ -1,11 +1,12 @@
-import { BellIcon, MagnifyingGlassIcon, TrashIcon, WrenchIcon } from '@heroicons/react/24/outline';
+import { TrashIcon, WrenchIcon } from '@heroicons/react/24/outline';
 import { useEffect, useState } from 'react';
-import MenuDropdown from '../components/ui/MenuDropdown';
+// import MenuDropdown from '../components/ui/MenuDropdown';
 import auth from '../utils/firebase.config';
 import { onAuthStateChanged } from 'firebase/auth';
 import { useGetUsersQuery, useRemoveUserMutation } from '../redux/features/users/usersApi';
 import UpdateUserModal from '../components/users/UpdateUserModal';
 import Swal from 'sweetalert2';
+import Navbar from '../components/layouts/Navbar';
 
 
 const admin = "aaaaaaaanikmojumder@gmail.com";
@@ -89,35 +90,7 @@ const Settings = () => {
         <UpdateUserModal isOpen={isOpen} setIsOpen={setIsOpen} id={userId} modalUser={modalUser}></UpdateUserModal>
         <div className="col-span-9 px-10 pt-10">
           {/* NavBars-------------------- */}
-          <div className="flex justify-between items-center">
-            <div>
-              <h1 className="font-semibold text-3xl">Setting</h1>
-            </div>
-            {/* NavLinks  */}
-            <div className="flex gap-5">
-              <button className="border-2 border-secondary/20 hover:border-primary hover:bg-primary rounded-xl h-10 w-10  grid place-content-center text-secondary hover:text-white transition-all">
-                <MagnifyingGlassIcon className="h-6 w-6" />
-              </button>
-              <button className="border-2 border-secondary/20 hover:border-primary hover:bg-primary rounded-xl h-10 w-10 grid place-content-center text-secondary hover:text-white transition-all">
-                <BellIcon className="h-6 w-6" />
-              </button>
-
-              <MenuDropdown>
-                <div className="h-10 w-10 rounded-xl overflow-hidden">
-                  <img
-                    title={user?.displayName}
-                    src={user?.photoURL ?
-                      user.photoURL
-                      :
-                      "https://images.unsplash.com/photo-1528892952291-009c663ce843?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=644&q=80"
-                    }
-                    alt="profile"
-                    className="object-cover h-full w-full "
-                  />
-                </div>
-              </MenuDropdown>
-            </div>
-          </div>
+            <Navbar title="Settings"></Navbar>
           {/* Settings main interface (Second colspan)-------------*/}
           <div className="grid grid-cols-1 gap-5 mt-10">
             <div className="relative h-screen overflow-y-auto">
